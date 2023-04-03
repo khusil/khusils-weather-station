61f2a66b70b1be003d6d339efcb1375b1a7a8e5c
khusil <khushilmanji@yahoo.com>
2023-04-03 17:10:37 -0400

    added event handlers added event listeners

--- assests/java.js
+++ assests/java.js
@@ -4,3 +4,32 @@ var listItemE1 = document.querySelector(".list-group");
 var cityCard = document.querySelector("#city-container");
 var clickForCity = document.querySelector(".list-group-item");
 var saveCities = JSON.parse(localStorage.getItem(".list-group")) || [];
+
+var getWeatherInfo = function (city) {
+    // format the github api url
+    var apiUrl =
+      "https://api.openweathermap.org/data/2.5/forecast?q=" +
+      city +
+      "&units=imperial&appid=8d1c76621b3c56e2c6ba4131cbdbfec9";
+  
+    // make a repo request to the url
+    fetch(apiUrl).then(function (response) {
+      response.json().then(function (data) {
+        var latitude = data.city.coord.lat;
+        var longitude = data.city.coord.lon;
+  
+        return fetch(
+          "https://api.openweathermap.org/data/2.5/uvi?lat=" +
+            latitude +
+            "&lon=" +
+            longitude +
+            "&appid=8d1c76621b3c56e2c6ba4131cbdbfec9"
+        ).then(function (uvResponse) {
+          uvResponse.json().then(function (uvData) {
+            displayWeather(data, city, uvData);
+          });
+        });
+      });
+    });
+  };
+  
